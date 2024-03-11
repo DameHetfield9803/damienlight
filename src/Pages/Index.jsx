095fd5e1a2032afc613ac89dc6de1da9bf7281f0
DameHetfield9803 <you@example.com>
2024-03-11 11:54:46 +0800

    yes

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -13,10 +13,10 @@ import es from "../assets/edusave.jpg"
 export default function Index() {
     return (
         <div
-            className={"rounded-sm container bg-gradient-to-r from-sky-100 to-teal-200 min-w-full min-h-screen py-2 subpixel-antialiased"}>
-            <nav className={"min-w-fit mb-2 mx-auto mt-3"}>
-                <div className={"container max-w-screen mx-auto"}>
-                    <ul className={"space-x-4 items-start inline-flex"}>
+            className={"rounded-sm container bg-gradient-to-r from-orange-100 to-cyan-200 min-w-full min-h-screen py-2 subpixel-antialiased"}>
+            <nav className={"min-w-fit mb-2 mx-auto"}>
+                <div className={"container max-w-screen-xl items-center justify-between mx-auto p-4"}>
+                    <ul className={"flex space-x-4 items-center justify-center mx-auto"}>
                         <li><a href={"https://www.linkedin.com/in/damien-foo-a2a246210/"} target={"_blank"} rel={"noreferrer"}><img src={linkedin} alt={"Linkedin"} width={190} className={"mr-4 ml-4"}/></a></li>
                         <li><a href={"https://www.github.com/DameHetfield9803"} target={"_blank"} rel={"noreferrer"}><img src={github} alt={"Github"} width={48} className={"rounded-full scale-110 mr-4"}/></a></li>
                         <li><a href={"https://instagram.com/dame_fdk/"} target={"_blank"} rel={"noreferrer"}><img src={insta} alt={"Instagram"} width={50} className={"rounded-2xl"}/></a></li>
@@ -40,7 +40,7 @@ export default function Index() {
             <p className={"text-center text-xl mb-2 mt-2"}><strong>Certifications</strong></p>
             <img src={ai} alt={"AI Ethics and Governance"} width={500} className={"flex mx-auto mb-8 rounded-2xl"}/>
             <img src={cH} alt={"Cloudhacks"} width={500} className={"flex mx-auto rounded-2xl mb-8 scale-y-110"}/>
-            <img src={es} alt={"Edusave Award 2019"} width={500} className={"flex mx-auto"}/>
+            <img src={es} alt={"Edusave Award 2019"} width={500} className={"flex mx-auto rounded-xl"}/>
         </div>
     )
 }
